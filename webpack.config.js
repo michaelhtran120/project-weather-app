be80ebb100c8e3aae75d753a293e5aec6155c34d
michaelhtran120 <michaelhtran120@gmail.com>
2021-06-23 15:51:22 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.export = {
+  mode: "development",
+  entry: ".src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
